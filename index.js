b5057645362c581e2b691d1a23d929f91e513809
Bucephalus <bhargabnath691@gmail.com>
2021-04-26 17:03:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://nest:nest@cluster0.i8sxu.mongodb.net/sarthi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// const auth = require('./routes/auth');
+
+app.get('/api', (req, res) => res.send('API working fine!'));
+
+// app.use('/api', auth);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}...`));
