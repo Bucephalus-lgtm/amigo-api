e1c2fcaf6676f65ec3639fd433d22eae6d1085cc
Bucephalus <bhargabnath691@gmail.com>
2021-04-26 23:49:09 +0530

    mongodb database connection refactored

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const db = require('./src/database');
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-
-mongoose.connect('mongodb+srv://nest:nest@cluster0.i8sxu.mongodb.net/sarthi?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
+db();
 
 const auth = require('./src/routes/public/auth');
 const product = require('./src/routes/public/products');
