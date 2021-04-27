29782f4054edaeb3367522f405a203820c7f7443
Bucephalus <bhargabnath691@gmail.com>
2021-04-27 16:21:37 +0530

    get

--- src/routes/public/query.js
+++ src/routes/public/query.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { send_queries, receive_queries } = require("../../controllers/query");
+const { send_queries, receive_queries, } = require("../../controllers/query");
 const router = express.Router();
 
 router.post("/send-queries", send_queries);
-router.post("/receive-queries", receive_queries);
-g
+router.get("/receive-queries", receive_queries);
+
 module.exports = router;
