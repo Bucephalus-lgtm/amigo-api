a1e8f11df180a19c8ad4c78073f460d01e2aaaef
Bucephalus <bhargabnath691@gmail.com>
2021-04-27 09:56:40 +0530

    receive order done

--- src/controllers/seller.js
+++ src/controllers/seller.js
@@ -1,3 +1,4 @@
+const { Order } = require('../models/order');
 const Seller = require('../models/seller');
 
 exports.get_all_sellers = async (req, res) => {
@@ -8,4 +9,11 @@ exports.get_all_sellers = async (req, res) => {
 exports.seller_by_id = async (req, res) => {
     const seller = await Seller.findById(req.params.id);
     return res.json({ seller });
+}
+
+exports.receive_orders = async (req, res) => {
+    const sellerId = req.params.id;
+    console.log(sellerId);
+    const ordersReceived = await Order.find({ 'products': { $elemMatch: { seller: sellerId } } });
+    return res.json({ ordersReceived });
 }
--- src/routes/public/seller.js
+++ src/routes/public/seller.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { get_all_sellers, seller_by_id } = require("../../controllers/seller");
+const { get_all_sellers, seller_by_id, receive_orders } = require("../../controllers/seller");
 const router = express.Router();
 
 router.get("/get/all/sellers", get_all_sellers);
 
 router.get("/seller/:id", seller_by_id);
+
+router.get('/receive/orders/:id', receive_orders);
 
 module.exports = router;
